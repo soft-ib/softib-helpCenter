650c6ed879a3acc771dd67c7831177f68ef6b3f9
unknown <dhiasaadlaui@gmail.com>
2021-06-20 09:57:02 +0100

    discover core service

--- src/main/java/com/softib/helpcenter/SoftIbApplication.java
+++ src/main/java/com/softib/helpcenter/SoftIbApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,6 +30,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableDiscoveryClient
 public class SoftIbApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/softib/helpcenter/controllers/RestControllerQuestions.java
+++ src/main/java/com/softib/helpcenter/controllers/RestControllerQuestions.java
@@ -3,6 +3,7 @@ package com.softib.helpcenter.controllers;
 import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 import com.softib.helpcenter.util.Constants;
 import com.softib.helpcenter.util.IUserService;
 
@@ -21,22 +25,29 @@ public class RestControllerQuestions {
 
 	@Autowired
 	IUserService userService;
-	
+
+	@Autowired
+	private EurekaClient eurekaClient;
+
+	private String coreServiceId ="core-service";
+
 	@GetMapping(value = "questions")
 	@ResponseBody
 	String showTestQuestion() {
-		return "Hello, Help center is working !!!!"+" User and roles : " +userService.getCurrentUserName() + " " +userService.getCurrentUserRole();
+		return "Hello, Help center is working !!!!" + " User and roles : " + userService.getCurrentUserName() + " "
+				+ userService.getCurrentUserRole();
 	}
-	
+
 	@GetMapping(value = "/from-core/users")
 	@ResponseBody
 	Object fromCore(@RequestHeader("Authorization") String token) {
+		Application application = eurekaClient.getApplication(coreServiceId);
+		InstanceInfo instanceInfo = application.getInstances().get(0);
+		String url = "http://" + instanceInfo.getIPAddr() + ":" + instanceInfo.getPort() + "/users";
 		RestTemplate restTemplate = new RestTemplate();
-		final String uri = Constants.CORE_BASE_PATH + "/users";
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Authorization", token);
 		HttpEntity<String> entity = new HttpEntity<>(null, headers);
-		return restTemplate.exchange(uri, HttpMethod.GET, entity, Object.class);
+		return restTemplate.exchange(url, HttpMethod.GET, entity, Object.class);
 	}
-	
 }
